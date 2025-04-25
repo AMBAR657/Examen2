7a2b8ff1e9ceb64996383e3a1d871776fd88fda3
Ambar Itzel <zarzaambar52@gmail.com>
2025-04-25 09:57:04 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,17 @@
+// src/App.jsx
+import React from "react";
 import { MovieProvider } from "./context/MovieContext";
-import MovieForm from "./components/MovieForm";
-import MovieFilters from "./components/MovieFilters";
 import MovieList from "./components/MovieList";
-import FavoriteList from "./components/FavoriteList";
 
-export default function App() {
+function App() {
   return (
     <MovieProvider>
-      <div className="max-w-3xl mx-auto p-6">
-        <h1 className="text-3xl font-semibold text-center text-blue-700 mb-6">
-          Películas
-        </h1>
-        
-        <MovieForm />
-        
-        <div className="my-6">
-          <MovieFilters />
-        </div>
-
-        <div className="my-6">
-          <FavoriteList />
-        </div>
-
+      <div className="App">
         <MovieList />
       </div>
     </MovieProvider>
   );
 }
+
+export default App;
+
